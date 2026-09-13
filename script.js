0721c369406841e5ec679987305e62282f794ebc
GOKULANANDHJ <gokulanandhj7@gmail.com>
2026-09-13 16:12:32 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load",()=>{setTimeout(()=>document.getElementById("loader").style.display="none",650)});
-const menu=document.querySelector(".menu"), nav=document.querySelector(".nav nav");
-menu?.addEventListener("click",()=>nav.classList.toggle("open"));
-document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener("click",()=>nav.classList.remove("open")));
-const video=document.querySelector(".hero-video");
-video?.addEventListener("error",()=>video.style.display="none");
